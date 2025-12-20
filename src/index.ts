667717aa82af2874b8ecc3ab02025c11ce70975c
Subham Mondal <subhamoffi@gmail.com>
2025-12-21 01:17:21 +0530

    Signup route completed with handling existing user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,62 @@
 import express from "express";
 import { WebSocketServer } from "ws";
+import { User, UserZodSchema } from "./schema/student";
+import { hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
+const SECRET = process.env.JWT_SECRET || "";
 const PORT = 3000;
-
+const SALT = 10;
 const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.send("Working fine");
+});
+
+app.post("/auth/signup", async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const { success, data, error } = UserZodSchema.safeParse({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    if (!success)
+      return res.status(400).json({
+        success: false,
+        error: "Invalid request schema",
+      });
+
+    const hashPassword = await hash(password, SALT);
+    const userExisted = await User.findOne({
+      email,
+    });
+    if (userExisted)
+      return res.status(400).json({
+        success: false,
+        error: "Email already exists",
+      });
+
+    const newUser = await User.create({
+      name: data.name,
+      email: data.email,
+      role: data.role,
+      password: hashPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      data: newUser,
+    });
+  } catch (error) {
+    console.log("Error \n", error);
+    res.status(500).send("Internal server error");
+  }
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Server is listening in port ${PORT}`);
--- src/schema/student.ts
+++ src/schema/student.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import z, { email } from "zod";
 import mongoose, { model, Schema } from "mongoose";
 
 dotenv.config();
@@ -12,11 +13,23 @@ const URL = process.env.MONGO_URL || "";
 
 mongoose.connect(URL);
 
+export const UserZodSchema = z
+  .object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+    role: z.enum(["student", "teacher"]),
+  })
+  .strict();
+
 export const UserSchema = new Schema({
   name: String,
   email: String,
   password: String,
-  role: ROLE,
+  role: {
+    type: String,
+    enum: ROLE,
+  },
 });
 
 export const User = model("user", UserSchema);
